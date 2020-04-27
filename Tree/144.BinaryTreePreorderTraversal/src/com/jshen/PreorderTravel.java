c13ee6f66eb5567b60a57a8fc046e559fb40e372
JSHEN9085 <jshen9085@gmail.com>
2020-04-26 20:46:15 -0400

    Preorder travel of tree

---
+++ Tree/144.BinaryTreePreorderTraversal/src/com/jshen/PreorderTravel.java
@@ -0,0 +1,35 @@
+package com.jshen;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class PreorderTravel {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if(root == null) return result;
+
+        Stack<TreeNode> treeStack = new Stack<>();
+        treeStack.push(root);
+
+        while(!treeStack.isEmpty()){
+            TreeNode current = treeStack.pop();
+            result.add(current.val);
+
+            if(current.right != null){
+                treeStack.push(current.right);
+            }
+            if(current.left != null){
+                treeStack.push(current.left);
+            }
+        }
+        return result;
+    }
+}
