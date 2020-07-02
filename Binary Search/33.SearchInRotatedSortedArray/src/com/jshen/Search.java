742fbe6acb63fcf700e5da49ea7532cac49939c0
JSHEN9085 <jshen9085@gamil.com>
2020-07-02 13:58:04 -0400

    Search in Rotated

--- Binary Search/33.SearchInRotatedSortedArray/src/com/jshen/Search.java
+++ Binary Search/33.SearchInRotatedSortedArray/src/com/jshen/Search.java
@@ -1,28 +1,45 @@
 package com.jshen;
 
 public class Search {
-    //version 1, find the rotation point first
+    //own solution at second trial
     public int search(int[] nums, int target) {
         if(nums == null || nums.length == 0) return -1;
+
         int start = 0;
         int end = nums.length - 1;
 
-        while(start < end){ //this while loop is finding the rotation point
-            int mid = start + (end - start)/2;
-            if(nums[mid] > nums[end]){ //if nums[mid] > nums[end], means rotation point is at right side of mid, so we set start = mid + 1 to narrow down the range again
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        } //after this loop start(index) is the index of the rotation point
+        while(start + 1 < end){
+            int mid = (end - start) / 2 + start;
 
-        int left = 0;
-        int right =
+            if(nums[mid] == target) return mid;
+
+            if(nums[start] < nums[mid]){ //this means start and mid is still at ascending order
+                if(nums[start] <= target && target <= nums[mid]){ //if target is also between start and mid, this will be traditional binary search
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else { //representing nums[start] > nums[mid], rotation point is sitting between start and mid, but it still means between mid and end is still ascending order
+                if(nums[mid] <= target && target <= nums[end]){ //if target is within mid and end, it is also the traditional binary search
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+
+        if(nums[start] == target) return start; //if target is at the rotation border we would need this
+        if(nums[end] == target) return end; //if target is at the rotation border we would need this
+        return -1;
     }
 
+//** same as other binary search questions, this question also try to narrow down the interval.
+//in the interval where the rotation point is located at, we try to find where the target is, between start and mid, or mid and end
+//and then keep narrowing down the interval
 
 
-//    version 2
+
+//    solution example
 //    public int search(int[] nums, int target) {
 //        if(nums == null || nums.length == 0) return -1;
 //
