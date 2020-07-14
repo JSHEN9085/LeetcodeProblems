0ad6e4845dd8d044e0d7bc1a70e0deff1c85b0a6
JSHEN9085 <jshen9085@gamil.com>
2020-07-13 22:16:02 -0400

    Guess Number

---
+++ Binary Search/374.GuessNumberHigherorLower/src/com/jshen/GuessGame.java
@@ -0,0 +1,28 @@
+package com.jshen;
+
+//The question provides an API to tell if the guessed number is greater, smaller or equal to the answer
+// * Forward declaration of guess API.
+// * @param  num   your guess
+// * @return 	     -1 if num is lower than the guess number
+// *			      1 if num is higher than the guess number
+// *               otherwise return 0
+// * int guess(int num);
+
+public class GuessGame {
+    public int guessNumber(int n) {
+        int start = 0;
+        int end = n;
+
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if(guess(mid) == 0){
+                return mid;
+            } else if(guess(mid) == 1){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+}
