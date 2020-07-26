1cb62abab802c2f64dfeb0f376b0500da6e66898
JSHEN9085 <jshen9085@gamil.com>
2020-07-26 13:38:04 -0400

    Longest Increasing Subsequence

--- Binary Search/300.LongestIncreasingSubsequence/src/com/jshen/Longest.java
+++ Binary Search/300.LongestIncreasingSubsequence/src/com/jshen/Longest.java
@@ -1,24 +1,50 @@
 package com.jshen;
 
+import java.util.Arrays;
+
 public class Longest {
 
     //version 1, O(n^2) solution
+//    public int lengthOfLIS(int[] nums) {
+//        int n = nums.length;
+//        int[] dp = new int[n]; //using an array to store each length up to each index
+//
+//        int maxLen = 0;
+//
+//        for(int i = 0; i < n; i++){
+//            int len = 1;
+//            for(int j = 0; j < i; j++){// the main idea is calculate max length that up to each index
+//                if(nums[j] < nums[i]){
+//                    len = Math.max(len, dp[j] + 1); //update the length if the current num is less than nums[i]
+//                }
+//            }
+//            dp[i] = len; //after the 2nd for loop, len is the maximum length that up to index i, so update dp[i] as the longest length
+//            if(dp[i] > maxLen) maxLen = dp[i]; //compare len and maxLen now, so we don't need to iterate dp[] to find out the max at the end
+//        }
+//        return maxLen;
+//    }
+
+    //version 2 O(nlogn)
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n]; //using an array to store each length up to each index
+        int[] dp = new int[n];
 
-        int maxLen = 0;
+        int len = 0;
+        for(int num : nums){
+            int index = Arrays.binarySearch(dp, 0, len, num);
+            //searching num from dp, range is from 0 to len
 
-        for(int i = 0; i < n; i++){
-            int len = 1;
-            for(int j = 0; j < i; j++){// the main idea is calculate max length that up to each index
-                if(nums[j] < nums[i]){
-                    len = Math.max(len, dp[j] + 1); //update the length if the current num is less than nums[i]
-                }
+            if(index < 0){
+                index = -(index + 1);
             }
-            dp[i] = len; //after the 2nd for loop, len is the maximum length that up to index i, so update dp[i] as the longest length
-            if(dp[i] > maxLen) maxLen = dp[i]; //compare len and maxLen now, so we don't need to iterate dp[] to find out the max at the end
+
+            dp[index] = num;
+
+            if(index == len){
+                len++;
+            }
         }
-        return maxLen;
+        return len;
     }
+
 }
