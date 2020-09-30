9cdda4c562fdaa7ce2dfaefedbb2fcdb1d37a704
JSHEN9085 <jshen9085@gmail.com>
2020-09-30 11:47:17 -0400

    jump game

---
+++ Array/55.JumpGame/src/com/jshen/JumpGame.java
@@ -0,0 +1,18 @@
+package com.jshen;
+
+public class JumpGame {
+    public boolean canJump(int[] nums){
+        int max = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(i > max){
+                return false;
+            }
+
+            max = Math.max(max, i + nums[i]);
+        }
+
+        return true;
+        //return max >= (nums.length - 1);
+    }
+}
