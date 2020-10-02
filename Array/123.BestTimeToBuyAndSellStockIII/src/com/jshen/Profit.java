35d9f1e87d7cfb3d8795cde87dae202b94414c23
JSHEN9085 <jshen9085@gmail.com>
2020-10-02 18:26:42 -0400

    stock III

--- Array/123.BestTimeToBuyAndSellStockIII/src/com/jshen/Profit.java
+++ Array/123.BestTimeToBuyAndSellStockIII/src/com/jshen/Profit.java
@@ -4,18 +4,19 @@ public class Profit {
     public int maxProfit(int[] prices){
         if(prices.length < 2 || prices == null) return 0;
 
-        int[] record = new int[2];
-        int profit = 0;
-        int buy = prices[0];
+        int buyOne = Integer.MAX_VALUE;
+        int sellOne = 0;
 
-        for(int i = 1; i < prices.length; i++){
-            if(prices[i - 1] >= prices[i]){
-                buy = prices[i];
-            }
-            if(prices[i - 1] < prices[i] && prices[i] > prices[i + 1]){
+        int buyTwo = Integer.MAX_VALUE;
+        int sellTwo = 0;
 
-            }
+        for(int i = 0; i < prices.length; i++){
+            buyOne = Math.min(buyOne, prices[i]);
+            sellOne = Math.max(sellOne, prices[i] - buyOne);
 
+            buyTwo = Math.min(buyTwo, prices[i] - sellOne);
+            sellTwo = Math.max(sellTwo, prices[i] - buyTwo);
         }
+        return sellTwo;
     }
 }
