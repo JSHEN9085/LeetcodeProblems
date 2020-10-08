7933b61633e5722d0de32d104ab893849dc8f827
JSHEN9085 <jshen9085@gmail.com>
2020-10-07 23:03:56 -0400

    level order dfs

--- Tree/102.BinaryTreeLevelOrderTraversal/src/com/jshen/LevelOrderTravel.java
+++ Tree/102.BinaryTreeLevelOrderTraversal/src/com/jshen/LevelOrderTravel.java
@@ -13,29 +13,50 @@ public class LevelOrderTravel {
         TreeNode(int x) { val = x; }
     }
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new LinkedList<>();
-        if(root == null) return result;
+//    public List<List<Integer>> levelOrder(TreeNode root) {
+//        List<List<Integer>> result = new LinkedList<>();
+//        if(root == null) return result;
+//
+//        Queue<TreeNode> treeQueue = new LinkedList<>();
+//        treeQueue.add(root);
+//
+//        while(!treeQueue.isEmpty()){
+//            int size = treeQueue.size();
+//            List<Integer> currentLevel = new ArrayList<>();
+//
+//            for(int i = 0; i < size; i++){
+//                TreeNode currentNode = treeQueue.poll();
+//                currentLevel.add(currentNode.val);
+//                if(currentNode.left != null){
+//                    treeQueue.add(currentNode.left);
+//                }
+//                if(currentNode.right != null){
+//                    treeQueue.add(currentNode.right);
+//                }
+//            }
+//            result.add(currentLevel);
+//        }
+//        return result;
+//    }
 
-        Queue<TreeNode> treeQueue = new LinkedList<>();
-        treeQueue.add(root);
+    public List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
 
-        while(!treeQueue.isEmpty()){
-            int size = treeQueue.size();
-            List<Integer> currentLevel = new ArrayList<>();
-
-            for(int i = 0; i < size; i++){
-                TreeNode currentNode = treeQueue.poll();
-                currentLevel.add(currentNode.val);
-                if(currentNode.left != null){
-                    treeQueue.add(currentNode.left);
-                }
-                if(currentNode.right != null){
-                    treeQueue.add(currentNode.right);
-                }
-            }
-            result.add(currentLevel);
-        }
-        return result;
+        helper(res, root, 0);
+        return res;
     }
+
+    private void helper(List<List<Integer>> res, TreeNode node, int height){
+        if(node == null) return;
+        //1st add a list into res, and then add the value of the node inside the sublist
+        if(height >= res.size()){
+            res.add(new LinkedList<>());
+        }
+        res.get(height).add(node.val);
+        helper(res, node.left, height+1);
+        helper(res, node.right, height+1); 
+    }
+
+
 }
