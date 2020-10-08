351a2b1723a68a9a43bf45bbbcdaffcd06546a03
JSHEN9085 <jshen9085@gmail.com>
2020-10-08 17:45:58 -0400

    postorder recursive

--- Tree/145.BinaryTreePostorderTraversal/src/com/jshen/PostorderTravel.java
+++ Tree/145.BinaryTreePostorderTraversal/src/com/jshen/PostorderTravel.java
@@ -1,6 +1,7 @@
 package com.jshen;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -12,23 +13,37 @@ public class PostorderTravel {
         TreeNode(int x) { val = x; }
     }
 
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if(root == null) return result;
+//    public List<Integer> postorderTraversal(TreeNode root) {
+//        List<Integer> result = new ArrayList<>();
+//        if(root == null) return result;
+//
+//        Stack<TreeNode> treeStack = new Stack<>();
+//        treeStack.push(root);
+//
+//        while(!treeStack.isEmpty()){
+//            TreeNode cur = treeStack.pop();
+//            result.add(0, cur.val); //if result is declared as linked list, use result.addFirst
+//            if(cur.left != null){
+//                treeStack.push(cur.left);
+//            }
+//            if(cur.right != null){
+//                treeStack.push(cur.right);
+//            }
+//        }
+//        return result;
+//    }
 
-        Stack<TreeNode> treeStack = new Stack<>();
-        treeStack.push(root);
+    //recursive
+    public List<Integer> postorderTraversal(TreeNode root){
+        LinkedList<Integer> res = new LinkedList<>();
+        helper(res, root);
+        return res;
+    }
 
-        while(!treeStack.isEmpty()){
-            TreeNode cur = treeStack.pop();
-            result.add(0, cur.val); //if result is declared as linked list, use result.addFirst
-            if(cur.left != null){
-                treeStack.push(cur.left);
-            }
-            if(cur.right != null){
-                treeStack.push(cur.right);
-            }
-        }
-        return result;
+    private void helper(LinkedList<Integer> res, TreeNode node){
+        if(node == null) return;
+        res.addFirst(node.val);
+        helper(res, node.right);
+        helper(res, node.left);
     }
 }
