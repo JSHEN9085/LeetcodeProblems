3e0fe443cd94b3010886a0992f07ed0a1a607aea
JSHEN9085 <jshen9085@gmail.com>
2020-10-13 11:57:00 -0400

    LCA

---
+++ Tree/235.LowestCommonAncestorOfABinarySearchTree/src/com/jshen/LowestCommonAncestor.java
@@ -0,0 +1,26 @@
+package com.jshen;
+
+public class LowestCommonAncestor {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        TreeNode cur = root;
+
+        while(cur != null){
+            if(p.val < cur.val && q.val < cur.val){
+                cur = cur.left;
+            } else if(p.val > cur.val && q.val > cur.val){
+                cur = cur.right;
+            } else {
+                return cur;
+            }
+        }
+        return null;
+    }
+}
