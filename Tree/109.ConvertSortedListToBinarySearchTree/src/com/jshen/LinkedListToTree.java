ca0d10854cf4a542d451846f13d761ad66c964c9
JSHEN9085 <jshen9085@gmail.com>
2020-10-14 23:38:37 -0400

    list to tree

--- Tree/109.ConvertSortedListToBinarySearchTree/src/com/jshen/LinkedListToTree.java
+++ Tree/109.ConvertSortedListToBinarySearchTree/src/com/jshen/LinkedListToTree.java
@@ -17,28 +17,51 @@ public class LinkedListToTree {
         public TreeNode(int x){val = x;}
     }
 
-    public TreeNode sortedListToBST(ListNode head) {
-        if(head == null) return null;
+//    public TreeNode sortedListToBST(ListNode head) {
+//        if(head == null) return null;
+//
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        while(head != null){
+//            arr.add(head.val);
+//            head = head.next;
+//        }
+//
+//        //we converted list to array;
+//
+//        return getTreeNode(arr, 0, arr.size() - 1);
+//    }
+//
+//    private TreeNode getTreeNode(List<Integer> arr, int start, int end){
+//        if(start > end) return null;
+//
+//        int mid = (end - start)/2 + start;
+//        TreeNode node = new TreeNode(arr.get(mid));
+//        node.left = getTreeNode(arr, start, mid-1);
+//        node.right = getTreeNode(arr, mid+1, end);
+//
+//        return node;
+//    }
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        while(head != null){
-            arr.add(head.val);
-            head = head.next;
+
+    public TreeNode sortedListToBST(ListNode head){
+        if(head == null) return null;
+        return getTreeNode(head, null);
+    }
+
+    private TreeNode getTreeNode(ListNode head, ListNode tail){
+        if(head == tail) return null;
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != tail && fast.next != tail){
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        //we converted list to array;
-
-        return getTreeNode(arr, 0, arr.size() - 1);
-    }
-
-    private TreeNode getTreeNode(List<Integer> arr, int start, int end){
-        if(start > end) return null;
-
-        int mid = (end - start)/2 + start;
-        TreeNode node = new TreeNode(arr.get(mid));
-        node.left = getTreeNode(arr, start, mid-1);
-        node.right = getTreeNode(arr, mid+1, end);
-
+        TreeNode node = new TreeNode(slow.val);
+        node.left = getTreeNode(head, slow);
+        node.right = getTreeNode(slow.next, tail);
         return node;
     }
+
 }
