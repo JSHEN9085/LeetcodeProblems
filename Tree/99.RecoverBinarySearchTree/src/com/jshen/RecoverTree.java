ca8ca8a79ce793db76dced7fe2cf92b5633cadc5
JSHEN9085 <jshen9085@gmail.com>
2020-10-16 22:13:50 -0400

    morris traversal

--- Tree/99.RecoverBinarySearchTree/src/com/jshen/RecoverTree.java
+++ Tree/99.RecoverBinarySearchTree/src/com/jshen/RecoverTree.java
@@ -10,33 +10,81 @@ public class RecoverTree {
         public TreeNode(int x){val = x;}
     }
 
+//    public void recoverTree(TreeNode root){
+//        if(root == null) return;
+//
+//        Stack<TreeNode> treeStack = new Stack<>();
+//        TreeNode first = null;
+//        TreeNode second = null;
+//        TreeNode pre = new TreeNode(Integer.MIN_VALUE);
+//        TreeNode cur = root;
+//
+//        while(cur != null || !treeStack.isEmpty()){
+//            while(cur != null){
+//                treeStack.push(cur);
+//                cur = cur.left;
+//            }
+//
+//            cur = treeStack.pop();
+//
+//            if(first == null && pre.val > cur.val) first = pre;
+//            if(first != null && pre.val > cur.val) second = cur;
+//
+//            pre = cur;
+//            cur = cur.right;
+//        }
+//
+//            int tem = first.val;
+//            first.val = second.val;
+//            second.val = tem;
+//
+//    }
+
+    //Morris Traversal O(1) space
     public void recoverTree(TreeNode root){
         if(root == null) return;
 
-        Stack<TreeNode> treeStack = new Stack<>();
         TreeNode first = null;
         TreeNode second = null;
-        TreeNode pre = new TreeNode(Integer.MIN_VALUE);
+        TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+
         TreeNode cur = root;
-
-        while(cur != null || !treeStack.isEmpty()){
-            while(cur != null){
-                treeStack.push(cur);
-                cur = cur.left;
+        while(cur != null){
+            if(cur.left != null){
+                TreeNode temp = cur.left;
+                while(temp.right != null && temp.right != cur){
+                    temp = temp.right;
+                }
+                if(temp.right == null){
+                    temp.right = cur;
+                    cur = cur.left;
+                } else {
+                    temp.right = null;
+                    if(prev.val > cur.val && first == null){
+                        first = prev;
+                    }
+                    if(prev.val > cur.val && first != null){
+                        second = cur;
+                    }
+                    prev = cur;
+                    cur = cur.right;
+                }
+            } else {
+                if(prev.val > cur.val && first == null){
+                    first = prev;
+                }
+                if(prev.val > cur.val && first != null){
+                    second = cur;
+                }
+                prev = cur;
+                cur = cur.right;
             }
-
-            cur = treeStack.pop();
-
-            if(first == null && pre.val > cur.val) first = pre;
-            if(first != null && pre.val > cur.val) second = cur;
-
-            pre = cur;
-            cur = cur.right;
         }
 
-            int tem = first.val;
+        if(first != null && second != null){
+            int swap = first.val;
             first.val = second.val;
-            second.val = tem;
-
+            second.val = swap;
+        }
     }
 }
