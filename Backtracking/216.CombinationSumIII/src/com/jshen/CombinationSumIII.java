94e47255bd405713e55fcac1582bac21ebd6607e
JSHEN9085 <jshen9085@gmail.com>
2020-10-22 22:33:42 -0400

    combination sum III

--- Backtracking/216.CombinationSumIII/src/com/jshen/CombinationSumIII.java
+++ Backtracking/216.CombinationSumIII/src/com/jshen/CombinationSumIII.java
@@ -1,12 +1,57 @@
 package com.jshen;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CombinationSumIII {
 //Only numbers 1 through 9 are used.
 //Each number is used at most once.
 
-    public List<List<Integer>> combinatioinSum3(int k, int n){
+//    public List<List<Integer>> combinationSum3(int k, int n){
+//        List<List<Integer>> res = new LinkedList<>();
+//        List<Integer> combine = new ArrayList<>();
+//        dfs(k, n, 1, res, combine);
+//        return res;
+//    }
+//
+//    private void dfs(int k, int n, int start, List<List<Integer>> res,
+//                     List<Integer> combine){
+//
+//        if(combine.size() == k && n == 0){
+//            res.add(new ArrayList<Integer>(combine));
+//            return;
+//
+//        }
+//
+//        for(int i = start; i <= 9; i++){
+//            combine.add(i);
+//            dfs(k, n - i, i+1, res, combine);
+//            combine.remove(combine.size() - 1);
+//        }
+//    }
 
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new LinkedList<>();
+        List<Integer> combo = new ArrayList<>();
+        dfs(k, n, 1, res, combo);
+        return res;
+    }
+
+    private void dfs(int k, int n, int start, List<List<Integer>> res, List<Integer> combo){
+        if(combo.size() == k && n == 0){
+            res.add(new ArrayList<Integer>(combo));
+            return;
+        }
+
+        if(start > 9 || combo.size() > k) return;
+
+        //not choose i
+        dfs(k, n, start + 1, res, combo);
+
+        //choose i
+        combo.add(start);
+        dfs(k, n - start, start + 1, res, combo);
+        combo.remove(combo.size() - 1);
     }
 }
