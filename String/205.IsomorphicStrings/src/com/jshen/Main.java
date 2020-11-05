82adaa7ea7ac8f3f76d80ded5df388438b280471
JSHEN9085 <jshen9085@gmail.com>
2020-11-05 17:30:52 -0500

    ASCII

--- String/205.IsomorphicStrings/src/com/jshen/Main.java
+++ String/205.IsomorphicStrings/src/com/jshen/Main.java
@@ -3,6 +3,14 @@ package com.jshen;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] map = new int[128];
+	    map['a'] = 20;
+	    map['c'] = 30;
+
+//	    for(int i = 0; i < map.length; i++){
+//            System.out.println(map[i]);
+//        }
+        System.out.println(map[99]);
+
     }
 }
--- String/205.IsomorphicStrings/src/com/jshen/Solution.java
+++ String/205.IsomorphicStrings/src/com/jshen/Solution.java
@@ -5,25 +5,48 @@ import java.util.HashMap;
 public class Solution {
 
     //time: O(2n), space: O(2n)
+//    public boolean isIsomorphic(String s, String t){
+//        if(s.length() != t.length()) return false;
+//
+//        return helper(s, t) && helper(t, s);
+//    }
+//
+//    private boolean helper(String s, String t){
+//        HashMap<Character, Character> match = new HashMap<>();
+//
+//        for(int i = 0; i < s.length(); i++){
+//            char charS = s.charAt(i);
+//            char charT = t.charAt(i);
+//
+//            if(match.containsKey(charS)){
+//                if(match.get(charS) != charT){
+//                    return false;
+//                }
+//            } else {
+//                match.put(charS, charT);
+//            }
+//        }
+//        return true;
+//    }
+
+    //ASCII, Time: O(n), space: O(1)
     public boolean isIsomorphic(String s, String t){
         if(s.length() != t.length()) return false;
 
-        return helper(s, t) && helper(t, s);
-    }
-
-    private boolean helper(String s, String t){
-        HashMap<Character, Character> match = new HashMap<>();
+        int[] mapS = new int[128];
+        int[] mapT = new int[128];
 
         for(int i = 0; i < s.length(); i++){
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
 
-            if(match.containsKey(charS)){
-                if(match.get(charS) != charT){
-                    return false;
-                }
+            if(mapS[cs] != mapT[ct]){
+                return false;
             } else {
-                match.put(charS, charT);
+                if(mapS[cs] == 0){
+                    mapS[cs] = i + 1;
+                    mapT[ct] = i + 1;
+                }
             }
         }
         return true;
