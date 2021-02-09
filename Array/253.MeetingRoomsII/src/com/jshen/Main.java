9ccdb3d6a2df7c0a97b1065842fc403fb12ec20f
JSHEN9085 <jshen9085@gmail.com>
2021-02-08 23:22:00 -0500

    meeting rooms II faster

--- Array/253.MeetingRoomsII/src/com/jshen/Main.java
+++ Array/253.MeetingRoomsII/src/com/jshen/Main.java
@@ -6,6 +6,6 @@ public class Main {
 	    int[][] input1 = {{0,30}, {5,10}, {15,20}};
         int[][] input2 = {{7,10}, {2,4}};
 	    MeetingRooms test = new MeetingRooms();
-        System.out.println(test.minMeetingRooms(input2));
+        System.out.println(test.minMeetingRooms(input1));
     }
 }
--- Array/253.MeetingRoomsII/src/com/jshen/MeetingRooms.java
+++ Array/253.MeetingRoomsII/src/com/jshen/MeetingRooms.java
@@ -14,23 +14,54 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class MeetingRooms {
+    //priority queue
+//    public int minMeetingRooms(int[][] intervals){
+//        if(intervals == null || intervals.length == 0) return 0;
+//
+//        //sorting based on starting time
+//        Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
+//
+//        //create priority queue
+//        PriorityQueue<int[]> pq = new PriorityQueue<>( (int[] a, int[] b) -> a[1] - b[1] );
+//        pq.offer(intervals[0]);
+//
+//        for(int i = 1; i < intervals.length; i++){
+//            if(intervals[i][0] >= pq.peek()[1]){
+//                pq.poll();
+//            }
+//            pq.offer(intervals[i]);
+//        }
+//        return pq.size();
+//    }
+
+    //2 arrays solution
     public int minMeetingRooms(int[][] intervals){
         if(intervals == null || intervals.length == 0) return 0;
 
-        //sorting based on starting time
-        Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
+        int res = 0; //final result: the number of rooms
 
-        //create priority queue
-        PriorityQueue<int[]> pq = new PriorityQueue<>( (int[] a, int[] b) -> a[1] - b[1] );
-        pq.offer(intervals[0]);
+        //assign 2 arrays to sort starting time and ending time
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
 
-        for(int i = 1; i < intervals.length; i++){
-            if(intervals[i][0] >= pq.peek()[1]){
-                pq.poll();
-            }
-            pq.offer(intervals[i]);
+        for(int i = 0; i < intervals.length; i++){
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
         }
-        return pq.size();
+
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        //check overlap
+        for(int i = 0, j = 0; i < intervals.length; i++){
+            if(start[i] >= end[j]){
+                j++;
+            } else {
+                res++;
+            }
+        }
+        return res;
     }
+
 
 }
