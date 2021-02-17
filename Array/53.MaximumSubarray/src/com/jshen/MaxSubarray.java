517c0ab536581e07d89ce8a8d609b11a36a405d7
JSHEN9085 <jshen9085@gmail.com>
2021-02-16 21:33:28 -0500

    max subarray

--- Array/53.MaximumSubarray/src/com/jshen/MaxSubarray.java
+++ Array/53.MaximumSubarray/src/com/jshen/MaxSubarray.java
@@ -2,6 +2,15 @@ package com.jshen;
 
 public class MaxSubarray {
     public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
 
+        int max = nums[0];
+        int curSum = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            curSum = Math.max(curSum + nums[i], nums[i]);
+            max = Math.max(max, curSum);
+        }
+        return max;
     }
 }
