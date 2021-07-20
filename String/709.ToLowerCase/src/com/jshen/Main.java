c3e5799dfc11f86cd83073d52d658eaea843a1a5
JSHEN9085 <jshen9085@gmail.com>
2021-07-19 22:33:29 -0400

    toLowerCase

---
+++ String/709.ToLowerCase/src/com/jshen/Main.java
@@ -0,0 +1,12 @@
+package com.jshen;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char upper = 'A';
+	    char lower = 'a';
+
+
+        System.out.println((char) (upper + 32));
+    }
+}
---
+++ String/709.ToLowerCase/src/com/jshen/Solution.java
@@ -0,0 +1,16 @@
+package com.jshen;
+
+public class Solution {
+
+    public String toLowerCase(String s){
+        if(s == null || s.length() == 0) return s;
+
+        char[] c = s.toCharArray();
+        for(int i = 0; i < s.length(); i++){
+            if(c[i] >= 'A' && c[i] <= 'Z'){
+                c[i] += 32;
+            }
+        }
+        return String.valueOf(c);
+    }
+}
