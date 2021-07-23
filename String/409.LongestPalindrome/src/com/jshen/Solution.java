9fa501321a1b500b76e3a03bacef0d73556ad447
JSHEN9085 <jshen9085@gmail.com>
2021-07-22 22:40:25 -0400

    Longest Palindrome

---
+++ String/409.LongestPalindrome/src/com/jshen/Solution.java
@@ -0,0 +1,31 @@
+package com.jshen;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    //time and space complexity is both O(n)
+//    public int longestPalindorme(String s){
+//        int res = 0;
+//        if(s.length() == 0 || s == null) return res;
+//
+//        Set<Character> count = new HashSet<>();
+//
+//        for(int i = 0; i < s.length(); i++){
+//            if(count.contains(s.charAt(i))){
+//                res+=2;
+//                count.remove(s.charAt(i));
+//            } else {
+//                count.add(s.charAt(i));
+//            }
+//        }
+//
+//        if(count.isEmpty()){
+//            return res;
+//        }
+//        return res + 1;
+//    }
+
+    
+}
